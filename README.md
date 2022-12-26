89f07778862ac271d031a33c5c3a55da3109ef3a
LiuShihao <liush99@foxmail.com>
2022-12-26 17:05:30 +0800

    kafkaClient

--- README.md
+++ README.md
@@ -62,10 +62,10 @@ cd /opt/kafka/bin
 #查看Topic列表
 kafka-topics.sh --list --zookeeper 192.168.153.131:2181 
 #查看Topic详情
-kafka-topics.sh --describe --topic test2 --zookeeper 192.168.153.131:2181
+kafka-topics.sh --describe --topic test --zookeeper 192.168.153.131:2181
 
 #创建Topic
-kafka-topics.sh --create --zookeeper 192.168.153.131:2181 --topic test2 --partitions 2 --replication-factor 1
+kafka-topics.sh --create --zookeeper 192.168.153.131:2181 --topic test --partitions 2 --replication-factor 1
 #删除Topic
 kafka-topics.sh --delete --zookeeper 192.168.153.131:2181 --topic test 
 
---
+++ src/main/java/com/lsh/simple/HelloKafkaProducer.java
@@ -0,0 +1,48 @@
+package com.lsh.simple;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Properties;
+
+/**
+ * 类说明：kafak生产者
+ */
+public class HelloKafkaProducer {
+
+    public static final String TOPIC_TEST = "test";
+
+    public static void main(String[] args) {
+        // 设置属性
+        Properties properties = new Properties();
+        // 指定连接的kafka服务器的地址
+        properties.put("bootstrap.servers","192.168.153.131:9092");
+        //补充一下： 配置多台的服务  用,分割， 其中一个宕机，生产者 依然可以连上（集群）
+        // 设置String的序列化 （对象-》二进制字节数组 ： 能够在网络上传输 ）
+        properties.put("key.serializer", StringSerializer.class);
+        properties.put("value.serializer", StringSerializer.class);
+
+        // 构建kafka生产者对象
+        KafkaProducer<String,String> producer  = new KafkaProducer<String, String>(properties);
+        try {
+            ProducerRecord<String,String> record;
+            try {
+                // 构建消息
+                record = new ProducerRecord<String,String>(TOPIC_TEST, "HelloWorld", LocalDateTime.now().format(DateTimeFormatter.BASIC_ISO_DATE));
+                // 发送消息
+                producer.send(record);
+                System.out.println("message is sent.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } finally {
+            // 释放连接
+            producer.close();
+        }
+    }
+
+
+}
